25ca2470e6c63b4623eaf43a5c526004b053e409
axton21 <axton_n@yahoo.com>
2015-05-07 16:19:51 -0400

    done

--- js/bike.js
+++ js/bike.js
@@ -1,7 +1,18 @@
-function Bike(){
-  // attributes go here...
+function Bike(name, biketype, framecolor){
+  this.name = name;
+  this.bikeType = biketype;
+  this.frameColor = framecolor;
+  this.WHEELS = 2;
+  this.status = 100;
+  this.condition = "ready to go!"
 }
 
-Bike.prototype.someInstanceMethod = function(){
-  // code goes here...
+Bike.prototype.takeForARide = function(num){
+  this.status -= 100;
+  if(this.status < 100){this.condition = "needs a tune up"};
 };
+
+Bike.prototype.tuneUp = function(){
+  this.status = 100;
+  this.condition = "ready to go!";
+}
--- js/owner.js
+++ js/owner.js
@@ -1,7 +1,21 @@
-function Owner(){
-  // attributes go here...
+function Owner(name){
+  this.name = name;
+  this.bikes = [];
 }
 
-Owner.prototype.someInstanceMethod = function(){
-  // code goes here...
+Owner.prototype.name = function(){
+  return this.name
+};
+
+Owner.prototype.buildBike = function(name, biketype, framecolor){
+  bike = new Bike(name, biketype, framecolor);
+  this.bikes.push(bike);
+};
+
+Owner.prototype.rideBike = function(bike, num){
+  for(var i = 0; i < this.bikes.length; i++){
+    if(this.bikes[i].name === bike){
+      this.bikes[i].takeForARide(num);
+    }
+  };
 };
